331b8ecbb9e6d8c994366d76cc9016608e23e166
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-11-05 18:00:17 +0100

    Add styles

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ use std::{
 use web_server::ThreadPool;
 
 fn main() {
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-  let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-  for stream in listener.incoming() {
-      let stream = stream.unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-      pool.execute(|| {
-          handle_connection(stream);
-      });
-  }
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -25,6 +25,8 @@ fn handle_connection(mut stream: TcpStream) {
 
     let (response, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 Ok", "hello.html")
+    } else if request_line == "GET /styles.css HTTP/1.1" {
+        ("HTTP/1.1 200 Ok", "styles.css")
     } else {
         ("HTTP/1.1 400 Not found", "404.html")
     };
