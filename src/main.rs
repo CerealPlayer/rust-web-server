7129ecbc5eb679d9ffe84ba12217f8711ee9d1dd
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-11-05 16:25:48 +0100

    Handle 404 routes

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use std::{
-  io::{prelude::*, BufReader},
-  net::{TcpListener, TcpStream},
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-  for stream in listener.incoming() {
-      let stream = stream.unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-      handle_connection(stream);
-  }
+        handle_connection(stream);
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-  let buf_reader = BufReader::new(&mut stream);
-  let http_request: Vec<_> = buf_reader
-      .lines()
-      .map(|result| result.unwrap())
-      .take_while(|line| !line.is_empty())
-      .collect();
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-  println!("Request: {:#?}", http_request);
+    let (response, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 Ok", "hello.html")
+    } else {
+        ("HTTP/1.1 400 Not found", "404.html")
+    };
+    let body = fs::read_to_string(filename).unwrap();
+    let length = body.len();
+    let response = format!("{response}\r\nContent-length: {length}\r\n\r\n{body}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
